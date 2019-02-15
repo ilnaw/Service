28ddaee626eafc901a477c8565f0e836d3f5b839
wanli <wanli@lefull.cn>
2019-02-15 14:55:32 +0800

    ignore

---
+++ TestService/TestService.h
@@ -0,0 +1,19 @@
+//
+//  TestService.h
+//  TestService
+//
+//  Created by wanli on 2019/2/15.
+//  Copyright © 2019 wanli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestService.
+FOUNDATION_EXPORT double TestServiceVersionNumber;
+
+//! Project version string for TestService.
+FOUNDATION_EXPORT const unsigned char TestServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestService/PublicHeader.h>
+
+
